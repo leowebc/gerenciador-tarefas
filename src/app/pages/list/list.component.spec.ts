ea5a41050332865a316a9790dc7349c8070e71e2
Leonardo Magalhaes <leowebc@gmail.com>
2025-01-02 16:03:46 -0300

    feat( completed-list )

--- src/app/pages/list/list.component.spec.ts
+++ src/app/pages/list/list.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ListComponent } from './list.component';
 import { By } from '@angular/platform-browser';
+import { Predicate, DebugElement } from '@angular/core';
 
 describe('ListComponent', () => {
   let component: ListComponent;
@@ -18,21 +19,30 @@ describe('ListComponent', () => {
 
   it('deve listar as tarefas', () => {
     
+    /*
     const todoSection = fixture.debugElement.query(By.css('#todo-list'));
-
     expect(todoSection).toBeTruthy();
-
-    const ulEl = todoSection.query(By.css('ul'));    
-    
-    
+    const ulEl = todoSection.query(By.css('ul'));   
     //const ulEl = fixture.debugElement.query(By.css('ul'));
-
-    expect(ulEl).toBeTruthy();
-    
+    expect(ulEl).toBeTruthy();    
     const liEls = ulEl.queryAll(By.css('li'));
+    expect(liEls.length).toBe(3);*/
 
-    expect(liEls.length).toBe(3);
+    const todoSection = fixture.debugElement.query(By.css('#todo-list'));
+    expect(todoSection).toBeTruthy();
+    const todoItems = todoSection.query(By.css('ul')).queryAll(By.css('li'));
+    expect(todoItems.length).toBe(3);
+
+
+    const completedSection = fixture.debugElement.query(By.css('#completed-list'));
+    expect(completedSection).toBeTruthy();
+    const completedItems = completedSection.query(By.css('ul')).queryAll(By.css('li'));
+    expect(completedItems.length).toBe(3);
 
     
   });
 });
+function yall(arg0: Predicate<DebugElement>) {
+  throw new Error('Function not implemented.');
+}
+
