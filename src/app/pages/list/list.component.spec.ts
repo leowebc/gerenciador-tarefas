339ed1d3aec4860c1492488ff4ca04cc7875268c
Leonardo Magalhães <leowebc@gmail.com>
2025-01-03 12:08:46 -0300

    novo html

--- src/app/pages/list/list.component.spec.ts
+++ src/app/pages/list/list.component.spec.ts
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ListComponent } from './list.component';
 import { By } from '@angular/platform-browser';
-import { Predicate, DebugElement } from '@angular/core';
-
 describe('ListComponent', () => {
   let component: ListComponent;
   let fixture: ComponentFixture<ListComponent>;
@@ -59,47 +56,3 @@ describe('ListComponent', () => {
     
   });
 });
-function yall(arg0: Predicate<DebugElement>) {
-  throw new Error('Function not implemented.');
-}
-
-=======
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ListComponent } from './list.component';
-import { By } from '@angular/platform-browser';
-
-describe('ListComponent', () => {
-  let component: ListComponent;
-  let fixture: ComponentFixture<ListComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ListComponent],
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(ListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('deve listar as tarefas', () => {
-    
-    const todoSection = fixture.debugElement.query(By.css('#todo-list'));
-
-    expect(todoSection).toBeTruthy();
-
-    const ulEl = todoSection.query(By.css('ul'));    
-    
-    
-    //const ulEl = fixture.debugElement.query(By.css('ul'));
-
-    expect(ulEl).toBeTruthy();
-    
-    const liEls = ulEl.queryAll(By.css('li'));
-
-    expect(liEls.length).toBe(3);
-
-    
-  });
-});
->>>>>>> origin/ListComponent
